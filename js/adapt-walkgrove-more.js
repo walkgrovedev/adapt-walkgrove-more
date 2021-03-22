1ccdcfc591fdb6847da5e457129bd2a401b1b1e3
@laura.lululand <laura.lululand@gmail.com>
2021-03-22 17:32:18 +0000

    initial build

--- js/adapt-walkgrove-more.js
+++ js/adapt-walkgrove-more.js
@@ -6,6 +6,12 @@ define([
 
   var moreView = ComponentView.extend({
 
+    events: {
+      'click .js-more-click': 'onMore'
+    },
+
+    moreIndex: -1,
+    
     preRender: function() {
       this.checkIfResetOnRevisit();
     },
@@ -21,7 +27,29 @@ define([
       if (isResetOnRevisit) {
         this.model.reset(isResetOnRevisit);
       }
+    },
+
+    onMore: function() {
+
+      this.$('.more__item-bg-graphic').addClass('is-hidden');
+      
+      this.moreIndex++;
+      this.model.get('_items').forEach((item, i) => {
+        if(this.moreIndex === i) {
+          this.$('.more__widget').eq(i).addClass('is-visible');
+        } else {
+          this.$('.more__widget').eq(i).removeClass('is-visible');
+        }
+        
+      });
+
+      if(this.moreIndex === this.model.get('_items').length-1) {
+        this.setCompletionStatus();
+        this.$('.more__btn').addClass('is-hidden');
+        //this.$('.component__instruction').html(Adapt.course.get('_globals').scrollInstruction);
+      }
     }
+
   },
   {
     template: 'more'
